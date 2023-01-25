d4fdf8974ee3ff76bfda69742e7e2c3dfe20a920
Oscar Vargas <oscar.vargas@adeccogroup.com>
2023-01-25 10:19:05 +0100

    Generic Class instead Vila

--- MagicVila_VilaAPI/Repository/IRepository/IVilaRepository.cs
+++ MagicVila_VilaAPI/Repository/IRepository/IVilaRepository.cs
@@ -5,11 +5,6 @@ namespace MagicVila_VilaAPI.Repository.IRepository
 {
     public interface IVilaRepository
     {
-        Task<Vila> GetAsync(Expression<Func<Vila,bool>> filter = null, bool tracked = true);
-        Task<List<Vila>> GetAllAsync(Expression<Func<Vila,bool>> filter = null);
-        Task CreateAsync(Vila entity);
         Task UpdateAsync(Vila entity);
-        Task RemoveAsync(Vila entity);
-        Task SaveAsync();
     }
 }
