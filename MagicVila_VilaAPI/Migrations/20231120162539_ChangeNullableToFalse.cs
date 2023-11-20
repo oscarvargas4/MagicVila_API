e2c975a9fd8aafa2f58e234f9de2a97dc2fc9bb2
Oscar Vargas <oscar.andres.vargas1@gmail.com>
2023-11-20 17:32:33 +0100

    CreateVila UI

---
+++ MagicVila_VilaAPI/Migrations/20231120162539_ChangeNullableToFalse.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilaVilaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeNullableToFalse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Amenity",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SpecialDetails",
+                table: "VilaNumbers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 11, 20, 17, 25, 39, 95, DateTimeKind.Local).AddTicks(6137));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 11, 20, 17, 25, 39, 95, DateTimeKind.Local).AddTicks(6206));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 11, 20, 17, 25, 39, 95, DateTimeKind.Local).AddTicks(6208));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2023, 11, 20, 17, 25, 39, 95, DateTimeKind.Local).AddTicks(6210));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2023, 11, 20, 17, 25, 39, 95, DateTimeKind.Local).AddTicks(6213));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Amenity",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SpecialDetails",
+                table: "VilaNumbers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 11, 7, 16, 28, 42, 926, DateTimeKind.Local).AddTicks(5189));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 11, 7, 16, 28, 42, 926, DateTimeKind.Local).AddTicks(5262));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 11, 7, 16, 28, 42, 926, DateTimeKind.Local).AddTicks(5264));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2023, 11, 7, 16, 28, 42, 926, DateTimeKind.Local).AddTicks(5267));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2023, 11, 7, 16, 28, 42, 926, DateTimeKind.Local).AddTicks(5269));
+        }
+    }
+}
--- MagicVila_Web/Controllers/VilaController.cs
+++ MagicVila_Web/Controllers/VilaController.cs
@@ -4,6 +4,7 @@ using MagicVila_Web.Models.Dto.VilaDto;
 using MagicVila_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace MagicVila_Web.Controllers
 {
@@ -16,15 +17,36 @@ namespace MagicVila_Web.Controllers
             _vilaService = vilaService;
             _mapper = mapper;
         }
+
         public async Task<IActionResult> IndexVila()
         {
             List<VilaDto> list = new();
             var response = await _vilaService.GetAllAsync<APIResponse>();
-            if (response != null)
+            if (response != null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<VilaDto>>(Convert.ToString(response.Result));
             }
             return View(list);
+        }
+
+        public async Task<IActionResult> CreateVila()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateVila(VilaCreateDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _vilaService.CreateAsync<APIResponse>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVila));
+                }
+            }
+            return View(model);
         }
     }
 }
