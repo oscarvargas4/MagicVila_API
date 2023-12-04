775f6ba30738608cb5e2b50824324f16ede56550
Oscar Vargas <oscar.andres.vargas1@gmail.com>
2023-12-04 18:52:00 +0100

    VilaNumber Update & Delete

---
+++ MagicVila_Web/Models/ViewModel/VilaNumberUpdateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MagicVila_Web.Models.Dto;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MagicVila_Web.Models.ViewModel
+{
+    public class VilaNumberUpdateViewModel
+    {
+        public VilaNumberUpdateViewModel()
+        {
+            VilaNumber = new VilaNumberUpdateDto();
+        }
+
+        public VilaNumberUpdateDto VilaNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> VilaList { get; set; }
+    }
+}
